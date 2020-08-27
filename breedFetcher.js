0abbdcb2dac0d420444964999e15113442236c3a
David Ramalho <david.m.ramalho@gmail.com>
2020-08-27 22:39:29 +0000

    tests added and tested?

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
 const request = require('request');
 //https://api.thecatapi.com/v1/breeds/search?q=sib
-const breedNameInput = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search' + '?q=' + breedNameInput, (error, response, body) => {
-  if (!error) {
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log(`Breed ${breedNameInput} not found.`);
+const fetchBreedDescription = (breedName, callback) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      if (!data[0]) {
+        callback(`Breed ${breedName} not found.`, null);
+      } else {
+        callback(null, data[0].description);
+      }
     } else {
-      console.log(data[0].description);
+      callback(`Oops! Something went wrong! ${error}`, null);
     }
-  } else {
-    console.log('Oops! Something went wrong! ', error);
-  }
-});
+  });
+  
+};
+
+module.exports = { fetchBreedDescription };
